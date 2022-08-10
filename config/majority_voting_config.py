fcd65d3de5fb9082926b353b0d7064d3ede2dec5
Christophe Pais <pais.christophe@gmail.com>
2022-08-10 15:38:00 +0100

    [Internal] Update DAO configuration following last team discussion (no change in product code)

--- config/majority_voting_config.py
+++ config/majority_voting_config.py
@@ -6,19 +6,19 @@ CONTRACT_METADATA_IPFS_LINK = "ipfs://QmNtph2DjrVcK9KXrNRsPSwMPpBpZjY7Ti6ceNcrbo
 MAIN_MAJ_CONTRACT_NAME = "AngryTeenagers Majority voting"
 MAIN_MAJ_DYNAMIC_INIT_VALUE = 2000
 MAIN_MAJ_GOVERNANCE_PARAMETERS = sp.record(vote_delay_blocks = sp.nat(1),
-                                           vote_length_blocks = sp.nat(180),
+                                           vote_length_blocks = sp.nat(14400),
                                            percentage_for_supermajority = sp.nat(80),
                                            fixed_quorum_percentage = sp.nat(25),
                                            fixed_quorum = sp.bool(False),
-                                           quorum_cap = sp.record(lower=sp.nat(1), upper=sp.nat(5800)))
+                                           quorum_cap = sp.record(lower=sp.nat(1), upper=sp.nat(5236)))
 
 
 
 OPTOUT_MAJ_CONTRACT_NAME = "AngryTeenagers Opt Out Majority voting"
 OPTOUT_MAJ_DYNAMIC_INIT_VALUE = 2000
 OPTOUT_MAJ_GOVERNANCE_PARAMETERS = sp.record(vote_delay_blocks = sp.nat(1),
-                                               vote_length_blocks = sp.nat(180),
-                                               percentage_for_supermajority = sp.nat(80),
+                                               vote_length_blocks = sp.nat(14400),
+                                               percentage_for_supermajority = sp.nat(50),
                                                fixed_quorum_percentage = sp.nat(25),
                                                fixed_quorum = sp.bool(True),
                                                quorum_cap = sp.record(lower=sp.nat(1), upper=sp.nat(5800)))
--- config/opt_out_voting_config.py
+++ config/opt_out_voting_config.py
@@ -4,5 +4,5 @@ CONTRACT_NAME = "AngryTeenagers Opt Out"
 ADMINISTRATOR_ADDRESS = "tz1QqobMeCYY1WjeaPUcphhyq2Q5C3BfTE2q"
 CONTRACT_METADATA_IPFS_LINK ="ipfs://QmV43FNJEJMGEzpAoMxmbpSbQBv3WKt4u4T6nd85it9R7j"
 GOVERNANCE_PARAMETERS = sp.record(vote_delay_blocks=sp.nat(1),
-                                  vote_length_blocks=sp.nat(180),
+                                  vote_length_blocks=sp.nat(14400),
                                   percentage_for_objection=sp.nat(10))

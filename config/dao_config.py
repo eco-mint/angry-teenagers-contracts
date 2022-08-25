0b55ac6a27ff5c108fff9d580292d57860003cba
Christophe Pais <pais.christophe@gmail.com>
2022-08-24 22:01:58 -0700

    [Internal] Update contract metadata link

--- config/dao_config.py
+++ config/dao_config.py
@@ -2,7 +2,7 @@ import smartpy as sp
 
 CONTRACT_NAME = "AngryTeenagers DAO"
 ADMINISTRATOR_ADDRESS = "tz1QqobMeCYY1WjeaPUcphhyq2Q5C3BfTE2q"
-CONTRACT_METADATA_IPFS_LINK = "ipfs://Qmbz6g7HQGUFqpGVEfYVi59gmZn8ETzfWYqbhZDhfk9mca"
+CONTRACT_METADATA_IPFS_LINK = "ipfs://QmSf2srebPb9ziamSNuXz4878Gt2LiqgrQCipm1uje6zQ8"
 
 # TODO: The opt out contract is not added. the majority one is not valid
 POLL_MANAGER_INIT_VALUE = sp.map(l = { 0 : sp.record(name=sp.string("MajorityVote"), address=sp.address("KT1Vk3GjhwHP732NcVBRT6yNmoAxy2Jyh81G")),

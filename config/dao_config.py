494ecd27ff0ea5ffb90a2fb710966833c1a7e23c
Christophe Pais <pais.christophe@gmail.com>
2022-10-26 14:18:50 +0100

    [Internal] Update contract metadata

--- config/dao_config.py
+++ config/dao_config.py
@@ -2,7 +2,7 @@ import smartpy as sp
 
 CONTRACT_NAME = "AngryTeenagers DAO"
 ADMINISTRATOR_ADDRESS = "tz1QqobMeCYY1WjeaPUcphhyq2Q5C3BfTE2q"
-CONTRACT_METADATA_IPFS_LINK = "ipfs://QmPWMNndeECJ6te9RgaM2G55NHike8NoDZrrjJ3YGzdVUh"
+CONTRACT_METADATA_IPFS_LINK = "ipfs://Qmdv2pjNJfhzga6tGf7cG83STZwo81rSoLwPsFXGGKioR1"
 
 # TODO: The opt out contract is not added. the majority one is not valid
 POLL_MANAGER_INIT_VALUE = sp.map(l = { 0 : sp.record(name=sp.string("MajorityVote"), address=sp.address("KT1Vk3GjhwHP732NcVBRT6yNmoAxy2Jyh81G")),
--- config/sale_config.py
+++ config/sale_config.py
@@ -3,4 +3,4 @@ import smartpy as sp
 CONTRACT_NAME = "AngryTeenagers Crowdsale contract"
 ADMINISTRATOR_ADDRESS = "KT1SNeLn9mp1f5dDyP1x4mnyAFY7wjHtJ7mt"
 MULTISIG_FUND_ADDRESS = "KT1Vpe5mJ6n2g9gxzmMCZNPjZkN5utnwZwd8"
-CONTRACT_METADATA_IPFS_LINK ="ipfs://QmWmhxSsPEYEkPKEaa4ttHBsZfYp5UUjpsJAJM3q96nGYf"
+CONTRACT_METADATA_IPFS_LINK ="ipfs://QmPge49xds6CyfzSteQzGAJSBZ3Hti1qqrXceeFfmqt1Lg"

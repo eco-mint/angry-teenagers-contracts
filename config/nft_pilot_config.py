f5090ea83c30ba893dc9354f05a582297fff4357
Christophe Pais <pais.christophe@gmail.com>
2023-03-21 20:24:01 +0000

    [Internal] Prepare the AngryTeenagers pilot project in Ghana. Contracts are not changed. Only their configuration (i.e their initial storage) is different

---
+++ config/nft_pilot_config.py
@@ -0,0 +1,46 @@
+import smartpy as sp
+
+CONTRACT_NAME = "AngryTeenagersPilot"
+ROYALTIES_BYTES ='{"decimals": 2, "shares": { "tz1QqobMeCYY1WjeaPUcphhyq2Q5C3BfTE2q": 10}}'
+CONTRACT_METADATA_IPFS_LINK ="ipfs://QmYemdPdJqfxzDBtBpnvAM6deqrw61VU3oFfNyCs7uJyio"
+
+# TODO
+ADMINISTRATOR_ADDRESS = "tz1QqobMeCYY1WjeaPUcphhyq2Q5C3BfTE2q"
+
+# TODO: This is not a valid generic image and displayUri is missing
+GENERIC_ARTWORK_IPFS_LINK = "ipfs://QmWkrkZj562duMGVwwaUtPo7iH1zPtLYKB2u9M7EfUYBDH"
+GENERIC_DISPLAY_ARTWORK_IPFS_LINK = "ipfs://QmWkrkZj562duMGVwwaUtPo7iH1zPtLYKB2u9M7EfUYBDH"
+GENERIC_THUMBNAIL_ARTWORK_IPFS_LINK = "ipfs://QmWkrkZj562duMGVwwaUtPo7iH1zPtLYKB2u9M7EfUYBDH"
+
+# TODO: A valid project oracles is needed as it cannot be changed later on
+PROJECT_ORACLES_STREAM_LINK = "ceramic://QmWkrkZj562duMGVwwaUtPo7iH1zPtLYKB2u9M7EfUYAAA"
+
+# TODO
+WHAT3WORDS_FILE_IPFS_LINK = "ipfs://QmWdJ3BTyEcQLZ4dVDaF6tf3FYnoqoXpMk5HAYs95E8dX6"
+MAX_SUPPLY=150
+
+# TODO: These values will have to adjusted to our artwork
+ARTIFACT_FILE_TYPE = '"image/png"'
+ARTIFACT_FILE_SIZE = '425118'
+ARTIFACT_FILE_NAME = '"angry_teenagers.png"'
+ARTIFACT_DIMENSIONS = '"2048x2048"'
+ARTIFACT_FILE_UNIT = '"px"'
+DISPLAY_FILE_TYPE = '"image/jpeg"'
+DISPLAY_FILE_SIZE = '143913'
+DISPLAY_FILE_NAME = '"angry_teenagers_display.jpeg"'
+DISPLAY_DIMENSIONS = '"2048x2048"'
+DISPLAY_FILE_UNIT = '"px"'
+THUMBNAIL_FILE_TYPE = '"image/jpeg"'
+THUMBNAIL_FILE_SIZE = '26875'
+THUMBNAIL_FILE_NAME = '"angry_teenagers_thumbnail.jpeg"'
+THUMBNAIL_DIMENSIONS = '"350x350"'
+THUMBNAIL_FILE_UNIT = '"px"'
+
+NAME_PREFIX = "Angry Teenager (Pilot) #"
+SYMBOL = "ATP"
+DESCRIPTION = "Angry Teenagers: tokens that plant forests and earn carbon offsets."
+LANGUAGE = "en-US"
+ATTRIBUTES_GENERIC = '[{\"name\"}, {\"generic\"}]'
+RIGHTS = "©2023 EcoMint. All rights reserved."
+CREATORS = '["KT1XmjJdFxUzuCJJVMyrXxS23hQrVtqSnhTH"]'
+PROJECTNAME = "Nsomyam Ye Reforestation - Pilot"
---
+++ main/nft_pilot_main.py
@@ -0,0 +1,44 @@
+import smartpy as sp
+
+NFT = sp.io.import_script_from_url("file:./nft/nft.py")
+Config = sp.io.import_script_from_url("file:./config/nft_pilot_config.py")
+
+########################################################################################################################
+########################################################################################################################
+# Compilation target
+########################################################################################################################
+########################################################################################################################
+sp.add_compilation_target(Config.CONTRACT_NAME,
+                          NFT.AngryTeenagers(
+                              administrator=sp.address(Config.ADMINISTRATOR_ADDRESS),
+                              royalties_bytes=sp.utils.bytes_of_string(Config.ROYALTIES_BYTES),
+                              metadata=sp.utils.metadata_of_url(Config.CONTRACT_METADATA_IPFS_LINK),
+                              generic_image_ipfs=sp.utils.bytes_of_string(Config.GENERIC_ARTWORK_IPFS_LINK),
+                              generic_image_ipfs_display=sp.utils.bytes_of_string(Config.GENERIC_DISPLAY_ARTWORK_IPFS_LINK),
+                              generic_image_ipfs_thumbnail=sp.utils.bytes_of_string(Config.GENERIC_THUMBNAIL_ARTWORK_IPFS_LINK),
+                              project_oracles_stream=sp.utils.bytes_of_string(Config.PROJECT_ORACLES_STREAM_LINK),
+                              what3words_file_ipfs=sp.utils.bytes_of_string(Config.WHAT3WORDS_FILE_IPFS_LINK),
+                              max_supply=Config.MAX_SUPPLY,
+                              artifact_file_type=Config.ARTIFACT_FILE_TYPE,
+                              artifact_file_size_generic=Config.ARTIFACT_FILE_SIZE,
+                              artifact_file_name=Config.ARTIFACT_FILE_NAME,
+                              artifact_dimensions=Config.ARTIFACT_DIMENSIONS,
+                              artifact_file_unit=Config.ARTIFACT_FILE_UNIT,
+                              display_file_type=Config.DISPLAY_FILE_TYPE,
+                              display_file_size_generic=Config.DISPLAY_FILE_SIZE,
+                              display_file_name=Config.DISPLAY_FILE_NAME,
+                              display_dimensions=Config.DISPLAY_DIMENSIONS,
+                              display_file_unit=Config.DISPLAY_FILE_UNIT,
+                              thumbnail_file_type=Config.THUMBNAIL_FILE_TYPE,
+                              thumbnail_file_size_generic=Config.THUMBNAIL_FILE_SIZE,
+                              thumbnail_file_name=Config.THUMBNAIL_FILE_NAME,
+                              thumbnail_dimensions=Config.THUMBNAIL_DIMENSIONS,
+                              thumbnail_file_unit=Config.THUMBNAIL_FILE_UNIT,
+                              name_prefix=Config.NAME_PREFIX,
+                              symbol=Config.SYMBOL,
+                              description=Config.DESCRIPTION,
+                              language=Config.LANGUAGE,
+                              attributes_generic=Config.ATTRIBUTES_GENERIC,
+                              rights=Config.RIGHTS,
+                              creators=Config.CREATORS,
+                              project_name=Config.PROJECTNAME))
